9a88ec3d8343af1de0f1593c6e8b907dadfb00fd
Faical Allou <faical.allou@gmail.com>
2017-05-27 00:02:47 +0100

    neutralize the enter button when sweet alert is up

--- static/js/customjs.js
+++ static/js/customjs.js
@@ -1,24 +1,26 @@
 function autoSuggestInput(inputfield, suggesttype){
-  //type can only be airport or city
-if (suggesttype == "airport") {var sourcetable = availableairports} else {var sourcetable = availablecities};
-$( function() {
-  $( "#"+inputfield ).autocomplete({
-        source: sourcetable,
-        minLength: 2,
-        response: function(event,ui)
-          {
-            if(ui.content.length==1)
+  swal_on = 0;
+    //type can only be airport or city
+  if (suggesttype == "airport") {var sourcetable = availableairports} else {var sourcetable = availablecities};
+  $( function() {
+    $( "#"+inputfield ).autocomplete({
+          source: sourcetable,
+          minLength: 2,
+          response: function(event,ui)
             {
-              document.getElementById(inputfield).value=ui.content[0].value;
-              ui.item = ui.content[0].value;
-            };
-            if(ui.content.length==0)
-            {swal(document.getElementById(inputfield).value+" is not a valid "+suggesttype.toUpperCase()+" code");
-              mixpanel.track("Error reset field", {"Page":viewpage, "input": document.getElementById(inputfield).value });
-              $(inputfield).val('');
-              $(inputfield).focus();
-            };
-          }
+              if(ui.content.length==1)
+              {
+                document.getElementById(inputfield).value=ui.content[0].value;
+                ui.item = ui.content[0].value;
+              };
+              if(ui.content.length==0)
+              { swal_on = 1;
+                swal({  title: "City code <> Airport code",  text: document.getElementById(inputfield).value.toUpperCase()+" is not a valid "+suggesttype.toUpperCase()+" code",  html: true, type: "warning",  confirmButtonText: "OK"}, function() {swal_on=0});
+                mixpanel.track("Error reset field", {"Page":viewpage, "input": document.getElementById(inputfield).value });
+                $(inputfield).val('');
+                $(inputfield).focus();
+              };
+            }
+    });
   });
-});
 };
