e17a6912cc59d696fd7904e4f101271f3db1a269
Faical Allou <faical.allou@gmail.com>
2017-07-25 23:50:26 +0100

    readded share on linkedin

--- static/js/sharelinkedin.js
+++ static/js/sharelinkedin.js
@@ -1,20 +1,18 @@
-
-
 // Setup an event listener to make an API call once auth is complete
 function onLinkedInLoad() {
 }
 
 // Handle the successful return from the API call
 function onSuccess(data) {
-  swal({
-    title:"Posted!",
-    text: "The post will be visible in few seconds on your profile <strong><a href="+data.updateUrl+" target='_blank'>here</a></strong>",
-    type: "success",
-    html: true
-    }
-  );
+  setTimeout( function () {
+    swal({
+      title:"Posted!",
+      text: "The post will be visible in few seconds on your profile <strong><a href="+data.updateUrl+" target='_blank'>here</a></strong>",
+      type: "success",
+      html: true
+      }
+    ), 2000});
   mixpanel.track("Sharing success", {"Page":viewpage, "Post":data.updateUrl});
-
 }
 
 // Handle an error response from the API call
@@ -30,20 +28,26 @@ function onError(error) {
         }
       );
       mixpanel.track("Sharing not logged in", {"Page":viewpage});
-
     }
     else
     {
-      swal("Error...", "Linkedin refused your post. Please check that you are logged in. We'll do what we can to fix this asap. "+error.message, "error");
+      swal({
+        title: "Error...", 
+        text: "Linkedin refused your post. </br><i>"+error.message+"</i> </br>We'll do what we can to fix this asap.",
+        type: "error",
+        html: true
+        }
+      );
       mixpanel.track("Sharing error", {"Page":viewpage, "Error": error.message});
     }
 }
 
 // Use the API call wrapper to share content on LinkedIn
 function shareContent(urlToPrint, subtitle) {
-  //traveltrendsURLencoded = encodeURIComponent("traveltrends.herokuapp.com/catchment_view?originairport="+window.originairport+"&rangekm="+window.rangekm+"&destinationcity="+window.destinationcity);
-  //screenshotURL = "http://api.screenshotmachine.com/?key=54021f&dimension=720x600&format=png&cacheLimit=0&timeout=3000&url=" + traveltrendsURL;
-  screenshotURL = "http://api.screenshotlayer.com/api/capture?access_key=3ea44b15b4158e350c0751b133a84b18&viewport=720x750&url="+urlToPrint;
+  urlEncoded = encodeURIComponent(urlToPrint);
+  screenshotURL = "http://api.screenshotmachine.com/?key=54021f&dimension=720x600&format=png&cacheLimit=0&timeout=3000&url=" + urlEncoded;
+  //screenshotURL = "http://api.screenshotlayer.com/api/capture?access_key=3ea44b15b4158e350c0751b133a84b18&viewport=720x750&url="+urlEncoded;
+  console.log(screenshotURL);
   swal({
     title: "Post this view to Linkedin",
     text: "This will be visible publicly on your profile",
@@ -52,7 +56,8 @@ function shareContent(urlToPrint, subtitle) {
     confirmButtonColor: "#5be2ed",
     confirmButtonText: "Post to my profile",
     inputPlaceholder: "add a comment",
-    closeOnConfirm: false
+    closeOnConfirm: false,
+    showLoaderOnConfirm: true,
     },
     function(inputTextPost){
         mixpanel.track("Sharing open", {"Page":viewpage});
