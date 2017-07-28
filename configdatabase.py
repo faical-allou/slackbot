64502fa8738cb30e0ae34b19c98983b544ae70f2
Faical Allou <faical.allou@gmail.com>
2017-07-28 19:06:43 +0100

    hiding password again

--- configdatabase.py
+++ configdatabase.py
@@ -1,2 +1,2 @@
 
-connectionStringDatabase = "ec2-54-235-125-135.compute-1.amazonaws.com' port='5432' dbname='d4sjjjfm3g35dc' user='swobzoejynjhpk' password='aJS-yO6EBUg6DgzVQSFwp3Ac1v'"
+connectionStringDatabase = "host='xxx port='xxx' dbname='xxx' user='xxx' password='xxx'"
