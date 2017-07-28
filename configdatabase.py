db8e9327db0c5e6a1d73f3354a69ed010bfc80e3
Faical Allou <faical.allou@gmail.com>
2017-07-28 19:01:57 +0100

    testing the gitignore

--- configdatabase.py
+++ configdatabase.py
@@ -1,3 +1,2 @@
 
-connectionStringDatabase = "xxx' port='xxx' dbname='xxx' user='xxx' password='xxxx'"
-     	
+connectionStringDatabase = "ec2-54-235-125-135.compute-1.amazonaws.com' port='5432' dbname='d4sjjjfm3g35dc' user='swobzoejynjhpk' password='aJS-yO6EBUg6DgzVQSFwp3Ac1v'"
