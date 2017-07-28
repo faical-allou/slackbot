76514854c70b9b52659f3057f6444361e85e7310
Faical Allou <faical.allou@gmail.com>
2017-07-28 18:46:13 +0100

    adding the variable for local

--- configdatabase.py
+++ configdatabase.py
@@ -1,3 +1,3 @@
 
-connectionStringDatabase = "host=xx' port='xx' dbname='xxx' user='xxx' password='xx'"
+connectionStringDatabase = "host='ec2-54-235-125-135.compute-1.amazonaws.com' port='5432' dbname='d4sjjjfm3g35dc' user='swobzoejynjhpk' password='aJS-yO6EBUg6DgzVQSFwp3Ac1v'"
      	
