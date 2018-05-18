1fa1840a06c2f3661073e63e29bb6e848231f759
Faical Allou <fallou@trv3119.local>
2018-05-18 15:12:13 +0200

    adding the database config file

--- app.py
+++ app.py
@@ -81,6 +81,6 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 80.
     port = int(os.environ.get('PORT', 5000))
     if os.environ.get('ON_HEROKU'):
-        app.run(host='0.0.0.0', port=port)
+        app.run(host='localhost', port=port)
     else :
         app.run(host='localhost', port=port)
